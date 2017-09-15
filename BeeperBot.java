8e25c0d82f5597a1ff52376bb8e41337a21f2d21
JuicedPantry <31353365+JuicedPantry@users.noreply.github.com>
2017-09-15 09:31:35 -0700

    I have completed my life

--- BeeperBot.java
+++ BeeperBot.java
@@ -10,9 +10,43 @@ public class BeeperBot extends Robot
     public BeeperBot(int st, int av, Direction dir, int numBeepers) {
         super(st, av, dir, numBeepers);
     }
-    
     public void runRace() {
-        
+        while(frontIsClear()){
+            collectBeeperSteeple();
+        }
     }
-   
+    public void collectBeeperSteeple(){
+        findBeepers();
+        pickUpBeepers();
+        findBeepers();
+        pickUpBeepers();
+        findBeepers();
+        pickUpBeepers();
+        findBeepers();
+        pickUpBeepers();
+        }
+    public void findBeepers() {
+        faceEast();
+        while(!nextToABeeper()){
+            move();
+        }
+    }
+    public void faceEast(){
+        while(!facingEast()){
+            turnLeft();
+        }
+    }
+    public void pickUpBeepers(){
+        turnLeft();
+        while(nextToABeeper()){
+            pickBeeper();
+            move();
+        }
+        turnLeft();
+        turnLeft();
+        while(frontIsClear()){
+            move();
+        }
+        turnLeft();
+    }
 }
